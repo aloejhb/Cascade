41a0469c5abea36003e4ac5fab29f441e3748a6c
Bo Hu <19688778+aloejhb@users.noreply.github.com>
2025-03-28 14:33:52 +0100

    Adjust package dependencies to be compatible with catrace

--- cascade2p/cascade.py
+++ cascade2p/cascade.py
@@ -469,7 +469,8 @@ def transfer_train_model(
 
 
 def predict(
-    model_name, traces, model_folder="Pretrained_models", threshold=0, padding=np.nan, trace_noise_levels=None, verbosity=1
+    model_name, traces, 
+    model_folder="Pretrained_models", threshold=0, padding=np.nan, verbosity=1, trace_noise_levels=None
 ):
 
     """Use a specific trained neural network ('model_name') to predict spiking activity for calcium traces ('traces')
@@ -508,9 +509,6 @@ def predict(
     padding : 0 or np.nan
         Value which is inserted for datapoints, where no prediction can be made (because of window around timepoint of prediction)
         Default value: np.nan, another recommended value would be 0 which circumvents some problems with following analysis.
-
-    trace_noise_levels: float (neurons x 1)
-        Noise levels of the traces. If not provided, the noise levels are calculated from the traces.
 
     vebosity : 0 or 1
         If set to 0, the output of predict() during inference in the console is suppressed
@@ -604,6 +602,7 @@ def predict(
     if trace_noise_levels is None:
         # calculate noise levels for each trace
         trace_noise_levels = utils.calculate_noise_levels(traces, sampling_rate)
+
 
     if verbose:
         print(
